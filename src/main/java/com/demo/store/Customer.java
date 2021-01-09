e28ca8c9723a4ac0e9e07f4ade0ef82c76dc0f64
jinyounglee <hs25823@naver.com>
2021-01-09 20:02:06 +0900

    1. statement 함수 내의 thisAmount 임시변수 제거 2. switch문 메서드 추출

--- src/main/java/com/demo/store/Customer.java
+++ src/main/java/com/demo/store/Customer.java
@@ -27,29 +27,9 @@ public class Customer {
 
         String result = getName() + " 고객님의 대여 기록 \n";
         while (rental.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rental.nextElement();
             // 비디오 종류별 대여료 계산
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
-
             // 적립 포인트를 1 포인트 증가
-
             frequentRenterPoints ++;
             // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
@@ -57,10 +37,10 @@ public class Customer {
 
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result = "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
+                    String.valueOf(each.getCharge()) + "\n";
 
             // 현재까지 누적된 초 대여료
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }
 
         // 푸터 행 추가
@@ -69,4 +49,6 @@ public class Customer {
 
         return result;
     }
+
+
 }
